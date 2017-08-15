2c9930e4306cce62426f15e8d0786f484d69cced
Venkatasai-mukesh <30889074+Venkatasai-mukesh@users.noreply.github.com>
2017-08-15 15:50:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article3',function(req, res){
      res.sendFile(path.join(__dirname,'ui','article3.html'));
 });
 app.get('/article4',function(req, res){
-     res.send('article 4 requested is served over here');
+     res.sendFile(path.join(__dirname,'ui','article4.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
