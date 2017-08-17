086dc3c7389e9eddbcad87fbd9c79d1a46b0b267
Venkatasai-mukesh <30889074+Venkatasai-mukesh@users.noreply.github.com>
2017-08-17 22:47:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,30 +6,36 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var content = {
+var article1 = {
     title: 'Article1 |Venkatasai Mukesh',
     heading: 'Article1',
     date: '15 August 2017',
     content:'<p>This is my first web page content This is my first web page content This is my first web page content This is my first web page content This is my first web page content This is my first web page content</p>'
 };
-
-var htmlTemplate = 
-    '<html>
-      <head>
-      <title>
-            Article1 |Venkatasai Mukesh
-      </title>
-           <link href="/ui/style.css" rel="stylesheet" />
-     </head>
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+var htmlTemplate ='
+      <html>
+         <head>
+            <title>
+              ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+          </head>
     <body>
         <div class="container">
         <div>
             <a href="/">Home</a>
         </div>
-        <hr>
-        <div>
+        <hr/>
+        <h3>
             ${heading}
-        </div>
+        </h3>
         <div>
           ${date}
         </div>
@@ -37,8 +43,9 @@ var htmlTemplate =
         ${content}
         </div>
     </div>
-    </body>
-</html>';
+  </body>
+</html>
+';
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
