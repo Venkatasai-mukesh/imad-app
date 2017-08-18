0e39b4aca2592219a2d167e21a9f4f9d0526e867
Venkatasai-mukesh <30889074+Venkatasai-mukesh@users.noreply.github.com>
2017-08-18 21:49:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,11 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 //move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 15;
+    img.style.marginLeft = marginLeft + 'px';
+}
        img.onclick = function () {
            var interval = setInterval(moveRight,100);
 };
