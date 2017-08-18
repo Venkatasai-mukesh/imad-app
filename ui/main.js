6accd0e59e42c474d89e6faf69c3cc65e417229c
Venkatasai-mukesh <30889074+Venkatasai-mukesh@users.noreply.github.com>
2017-08-18 21:31:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 //chnage the text of main-text of div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+//move the image
+var img = document.getElementById('img');
+img.onclick = function() {
+    img.style.marginleft="100px";
+};
