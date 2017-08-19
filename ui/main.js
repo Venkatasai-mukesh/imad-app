d5ba2cbabc7b8226ad5696e80bbfbbc64c40ce71
Venkatasai-mukesh <30889074+Venkatasai-mukesh@users.noreply.github.com>
2017-08-19 12:18:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,8 +7,6 @@ var button = document.getElementById("counter");
               //create a request object
               var request = new XMLHttpRequest();
             
-
-
     //capture the response and store it in a variable
     request.onreadystatechange = function () {
             if(request.readyState === XMLHttpRequest.DONE) {
@@ -26,7 +24,6 @@ var button = document.getElementById("counter");
     request.open('GET' , 'http://mukeshguniganti98.imad.hasura-app.io/counter' ,true );
     request.send(null);
     
-    
   };
   
   //submit name
@@ -34,16 +31,29 @@ var button = document.getElementById("counter");
   var name = nameInput.value;
   var submit = document.getElementById('submit_btn');
   submit.onclick = function () {
-  //make a request to the server and send the name
-  
-  
-  //capture a list of names and render its as a list
-   var names = ['name1', 'name2', 'name3' , 'name4'] ;
-   var list = '';
-   for(var i=0; i< names.length; i++){
-       list += '<li>' + names[i] + '</li>';
-   }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+                   //create a request object
+              var request = new XMLHttpRequest();
+            
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+            if(request.readyState === XMLHttpRequest.DONE) {
+             //Take some action
+                if(request.status === 200) {
+                 //capture a list of names and render its as a list
+                   var names = request.responseText;
+                   names = JSON.parse(names);
+                   var list = '';
+                   for(var i=0; i< names.length; i++){
+                       list += '<li>' + names[i] + '</li>';
+                   }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
+             }
+        }
+        //request not done yet
+    };
+    //make request
+    request.open('GET' , 'http://mukeshguniganti98.imad.hasura-app.i0/submit-name?name='+name ,true );
+    request.send(null);
   };
       
