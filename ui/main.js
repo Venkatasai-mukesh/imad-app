fcfa4b4e00ba61961f44850e5035a547f5e90cdd
Venkatasai-mukesh <30889074+Venkatasai-mukesh@users.noreply.github.com>
2017-08-19 12:46:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,7 +43,7 @@ var button = document.getElementById("counter");
                    names = JSON.parse(names);
                    var list = '';
                    for (var i=0; i< names.length; i++){
-                       list += '<li>' + names[i] + '</li>';
+                       list + '<li>' + names[i] + '</li>';
                    }
                     var ul = document.getElementById('namelist');
                     ul.innerHTML = list;
