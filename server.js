5ab2889152ed2e206a5730fe9ae05a591e0dcb3b
Venkatasai-mukesh <30889074+Venkatasai-mukesh@users.noreply.github.com>
2017-08-19 11:53:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
  
  var names = [];
-app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) { //URL :/submit-name?name=xxxx
     //get the name from request object
     var name = req.params.name;
     names.push(name);
